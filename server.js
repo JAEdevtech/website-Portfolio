3c5f3c55447df18ee83fdb026f8b677976cbe719
JAEdevtech <jae.ed.dev@gmail.com>
2026-02-14 23:11:30 +0200

    responsiveness added to cards and backgorund image

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/docs")));
 
 app.listen(port, () => {
   console.log(`PORT:${port} running...`);
